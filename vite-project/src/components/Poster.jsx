34949141ed707b1e9cbb77ca557b58d026c30df3
gambit-23 <sahilsharmapv@gmail.com>
2025-04-03 14:18:22 +0530

    Updated poster api key

--- vite-project/src/components/Poster.jsx
+++ vite-project/src/components/Poster.jsx
@@ -2,22 +2,28 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
 function Banner() {
-
+  const apiKey = import.meta.env.VITE_API_KEY;
   const [bannerMovie, setBannerMovie] = useState();
   useEffect(() => {
-  axios.get(
-        `https://api.themoviedb.org/3/movie/popular?api_key=032c26d18509d388e79f088006ff324d&language=en-US&page=1`)
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
+      )
       .then((res) => {
         setBannerMovie(res.data.results[0]);
-  });
-}, [] ); // this will run only when the component mounts, not on every render
+      });
+  }, []); // this will run only when the component mounts, not on every render
   console.log(bannerMovie);
 
   return (
     <div
       className="h-[20vh] md:h-[75vh] bg-cover bg-center flex items-end"
       style={{
-        backgroundImage: `url('${bannerMovie?.backdrop_path ? `https://image.tmdb.org/t/p/original/${bannerMovie?.backdrop_path}` : ""})`,
+        backgroundImage: `url('${
+          bannerMovie?.backdrop_path
+            ? `https://image.tmdb.org/t/p/original/${bannerMovie?.backdrop_path}`
+            : ""
+        })`,
       }}
     >
       <div className="text-white text-xl text-center w-full bg-gray-900/60 p-2">
